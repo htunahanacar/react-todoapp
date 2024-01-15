7dca93b5c888c3867f942ba7c4f22d2b54253c49
htunahanacar <htacar528@gmail.com>
2024-01-15 14:56:54 +0300

    Fix direct all links to Home

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={RootLayout()}>
       <Route index element={<Home />} />
+      <Route path="*" element={<Home />} />
     </Route>
   )
 );
